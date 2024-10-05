d123caa3910316e64946dd2a08fc33d149ce769d
Manthan11194 <jadhavmanthan2001@gmail.com>
2024-10-05 12:05:29 +0530

    commit

---
+++ Star Pattern/src/test.java
@@ -0,0 +1,18 @@
+
+public class test {
+
+	int a=10; String b="manthan";
+	void show()
+	{
+		
+		System.out.println(a+" "+b);
+	}
+	 
+	public static void main(String[] args) {
+		
+		test X =new test();
+		X.show();
+
+	}
+
+}
